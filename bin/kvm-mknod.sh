ea9252fbb5310123ccfbc8335a0441b60d7dbeb0
hawky-4s- <hawky-4s-@users.noreply.github.com>
2015-09-23 13:51:42 +0200

    chore(kvm): always set permissions for current grp for kvm
    related to #CAM-4501

--- bin/kvm-mknod.sh
+++ bin/kvm-mknod.sh
@@ -3,8 +3,20 @@
 
 set -e
 
+function ensureCorrectPermissions() {
+  # ensure that kvm group has correct id
+  NODE_GROUP=$(ls -n /dev/kvm | cut -d " " -f 4)
+  KVM_GROUP=$(getent group kvm | cut -d ":" -f 3)
+
+  if [ $NODE_GROUP -ne $KVM_GROUP ]; then
+    echo >&2 "Change kvm group id from $KVM_GROUP to $NODE_GROUP"
+    groupmod -g $NODE_GROUP kvm
+  fi
+}
+
 lsmod | grep '\<kvm\>' > /dev/null || {
   echo >&2 "KVM module not loaded; software emulation will be used"
+  ensureCorrectPermissions
   exit 1
 }
 
@@ -12,6 +24,7 @@ if [ ! -e /dev/kvm ]; then
   mknod /dev/kvm c 10 $(grep '\<kvm\>' /proc/misc | cut -f 1 -d' ') || {
     echo >&2 "Unable to make /dev/kvm node; software emulation will be used"
     echo >&2 "(This can happen if the container is run without -privileged)"
+    ensureCorrectPermissions
     exit 1
   }
 fi
@@ -19,13 +32,5 @@ fi
 dd if=/dev/kvm count=0 2>/dev/null || {
   echo >&2 "Unable to open /dev/kvm; qemu will use software emulation"
   echo >&2 "(This can happen if the container is run without -privileged)"
+  ensureCorrectPermissions
 }
-
-# ensure that kvm group has correct id
-NODE_GROUP=$(ls -n /dev/kvm | cut -d " " -f 4)
-KVM_GROUP=$(getent group kvm | cut -d ":" -f 3)
-
-if [ $NODE_GROUP -ne $KVM_GROUP ]; then
-  echo >&2 "Change kvm group id from $KVM_GROUP to $NODE_GROUP"
-  groupmod -g $NODE_GROUP kvm
-fi
